db3530fa3a2bacd822cd353653e05cd97620fb8a
shadow0911 <yoyofahimmonaemkhandj@gmail.com>
2022-05-16 17:12:08 +0600

    v1

--- commands/Owner/say.js
+++ commands/Owner/say.js
@@ -1,90 +1,98 @@
 const Discord = require('discord.js');
-module.exports = {
-    name: 'say',
-    aliases: ['copycat', 'repeat'],
-    description:'returns the arguement',
-    category: "Owner",
-    
-    run: async(client, message, args,prefix) =>{
-        if(message.author.id !== "571964900646191104"){
-            return
-        }
-        message.delete()
-        
-        if(!message.guild.me.permissions.has("SEND_MESSAGES", "MANAGE_MESSAGES")){
-            return
-        }
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
-        // let item = message.content.split(/--/g).join(' ')
-        // let item2 = item.split(/\s+/g).slice()
-        // let chan = item2.indexOf("channel")+1
-
-        // let chann = item2[chan]
-        // const channels = message.guild.channels.cache.find(c => c.id == chann.replace('<#','').replace('>',''))
-
-        // channels.send(item2[chan+1])
-
-        if(!args.length){
-            return message.channel.send({embeds: [new Discord.MessageEmbed()
-                .setAuthor("Command: Say", client.user.displayAvatarURL({
-                    dynamic: true , format: 'png'
-                }))
-                .setDescription('Bot returns the Arguments')
-                .setTitle('Aliases: copycat, repeat')
-                .addField('Commands :', [
-                    '• say',
-                    '• <@channel/ID>',
-                    '• embed'
-                ])
-                .addField('Usage: ', [
-                    `${prefix}say <your message>`,
-                    `${prefix}say <@channel/ID>`,
-                    `${prefix}say embed <your message>`
-                ])
-                .addField('Example: ', [
-                    `${prefix}say Hi!`,
-                    `${prefix}say #announcement Huge Announcement!`,
-                    `${prefix}say embed Hello everyone!`
-                ])
-                .setColor('#9000ff')
-            ]
-            })
-        }
-        async function sayMessage(value, Msg, mentionChannel){
-            switch(value){
-                case 'normal':
-                    await message.channel.send(Msg)
-                    break;
-                
-                case 'channel':
-                    await mentionChannel.send(Msg)
-                    break;
-
-                case 'embed':
-                    const Embed = new Discord.MessageEmbed()
-                    .setDescription(Msg)
-                    .setColor(message.guild.me.displayColor)
-                    await message.channel.send({embeds: [Embed]})
-            }
-        }
-
-        const valueMessage = args.slice(1).join(' ');
-        const regularMsg = args.slice(0).join(' ');
-       
-        const cmd = message.content.split(" ")[1]
-        const channel = message.guild.channels.cache.find(c => c.id == cmd.replace('<#','').replace('>',''))
-
-        switch(cmd){
-            case 'embed':
-                sayMessage('embed', valueMessage)
-                break;
-            
-            case `${channel}`:
-                sayMessage('channel', valueMessage, channel)
-                break;
-            
-            default:
-                sayMessage('normal', regularMsg)
-        }
+class SayManager{
+    constructor(client, guild, interaction){
+        this.client = client;
+        this.guild = guild;
+        this.interaction = interaction;
     }
+
+    Mainframe(content, channel){
+        if(channel){
+            return this.ChannelResolve(channel, content);
+        }
+
+        let embed = new Discord.MessageEmbed()
+            .setDescription(`Your message was sent`)
+            .setColor("#2f3136")
+
+        this.interaction.channel.send({content: content}).catch(err => {return console.log(err.stack)})
+
+        return this.errorhandle(embed);
+    }
+
+    ChannelResolve(channel, content){
+        let embed = new Discord.MessageEmbed()
+        if(channel.type !== "GUILD_TEXT"){
+
+            embed.setDescription("You can only send messages in Text-channles")
+            embed.setColor("RED")
+
+            return this.errorhandle(embed);
+        }
+
+        const hasPermissionInChannel = channel
+            .permissionsFor(this.interaction.member)
+            .has(['SEND_MESSAGES', 'VIEW_CHANNEL'], false);
+
+        const BotHasPermissionInChannel = channel
+            .permissionsFor(this.client.user)
+            .has(['SEND_MESSAGES', 'VIEW_CHANNEL'], false);
+
+        console.log(hasPermissionInChannel)
+        if(!hasPermissionInChannel){
+
+            embed.setDescription("You don't have permission to send message in "+channel.toString())
+            embed.setColor("RED")
+
+            return this.errorhandle(embed);
+        }
+
+        if(!BotHasPermissionInChannel){
+
+            embed.setDescription("I don't have permission to send meassage")
+            embed.setColor("RED")
+
+            return this.errorhandle(embed);
+        }
+        
+        channel.send({content: content}).catch(err => {return console.log(err.stack)});
+
+        embed.setDescription(`Your message was sent in ${channel.toString()}`);
+        embed.setColor("#2f3136")
+
+        return this.errorhandle(embed);
+    }
+
+    errorhandle(embed){
+        this.interaction.reply({embeds: [embed], ephemeral: true});
+    }
+}
+
+module.exports.run = {
+    run: (client, interaction) => {
+        const {options} = interaction;
+        let content = options.getString('message');
+        let channel = options.getChannel('channel');
+
+        let data = new SayManager(client, interaction.guild, interaction).Mainframe(content, channel)
+    }
+}
+module.exports.slash = {
+    data: new SlashCommandBuilder()
+        .setName('say')
+        .setDescription("Type a message and bot will send the message")
+        .addStringOption(Option => 
+            Option.setName('message')
+            .setDescription("The message you want bot to say")
+            .setRequired(true)
+        )
+        .addChannelOption(option =>
+            option.setName('channel')
+            .setDescription("Send message in another channel")),
+
+    category: "Administration",
+    Permissions: ["ADMINISTRATOR"],
+    ClientPermissions: ["SEND_MESSAGES"],
 }
