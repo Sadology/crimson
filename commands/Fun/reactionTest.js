7188a8644aaefd634f64193572ab7e0bf8865aa0
shadow0911 <yoyofahimmonaemkhandj@gmail.com>
2022-08-31 15:18:19 +0600

    New channel for animated emoji

--- commands/Fun/reactionTest.js
+++ commands/Fun/reactionTest.js
@@ -1,66 +1,66 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const {MessageEmbed, MessageActionRow, MessageButton} = require('discord.js');
-const wait = require('util').promisify(setTimeout);
+// const { SlashCommandBuilder } = require('@discordjs/builders');
+// const {MessageEmbed, MessageActionRow, MessageButton} = require('discord.js');
+// const wait = require('util').promisify(setTimeout);
 
-const row = new MessageActionRow()
-    .addComponents(
-        new MessageButton()
-        .setCustomId('reaction')
-    )
+// const row = new MessageActionRow()
+//     .addComponents(
+//         new MessageButton()
+//         .setCustomId('reaction')
+//     )
 
-class ReactiontestManager{
-    constructor(client, guild, interaction) {
-        this.client = client;
-        this.guild = guild;
-        this.interaction = interaction;
-    }
+// class ReactiontestManager{
+//     constructor(client, guild, interaction) {
+//         this.client = client;
+//         this.guild = guild;
+//         this.interaction = interaction;
+//     }
 
-    TimeGenerator(){
-        let Number = Math.floor(Math.random() * 8) + 1
+//     TimeGenerator(){
+//         let Number = Math.floor(Math.random() * 8) + 1
 
-        return Number;
-    };
+//         return Number;
+//     };
 
-    async ClickButtonTest(){
-        this.interaction.deferReply();
-        await wait(1000);
+//     async ClickButtonTest(){
+//         this.interaction.deferReply();
+//         await wait(1000);
 
-        row.components[0].setLabel('Wait...')
-        row.components[0].setStyle('PRIMARY')
+//         row.components[0].setLabel('Wait...')
+//         row.components[0].setStyle('PRIMARY')
 
-        this.interaction.editReply({embeds: [
-            new MessageEmbed()
-            .setDescription("Click the Blue button as fast as possible when the button turns green")
-        ], components: [row]})
-    }
-    Mainframe(type){
-        switch(type){
-            case 'click-button':
-                this.ClickButtonTest();
-            break;
-        }
-    }
-}
+//         this.interaction.editReply({embeds: [
+//             new MessageEmbed()
+//             .setDescription("Click the Blue button as fast as possible when the button turns green")
+//         ], components: [row]})
+//     }
+//     Mainframe(type){
+//         switch(type){
+//             case 'click-button':
+//                 this.ClickButtonTest();
+//             break;
+//         }
+//     }
+// }
 
-module.exports.run = {
-    run: async(client, interaction ) =>{
-        const {options} = interaction;
+// module.exports.run = {
+//     run: async(client, interaction ) =>{
+//         const {options} = interaction;
         
-        let type = options.getSubcommand();
+//         let type = options.getSubcommand();
         
-        let data = new ReactiontestManager(client, interaction.guild, interaction).Mainframe(type);
-    }
-}
+//         let data = new ReactiontestManager(client, interaction.guild, interaction).Mainframe(type);
+//     }
+// }
 
-module.exports.slash = {
-    data: new SlashCommandBuilder()
-        .setName('reaction-test')
-        .setDescription("Take a test on how fast your reaction")
-        .addSubcommand(option =>
-            option
-            .setName('click-button')
-            .setDescription("Click the button as fast it turns green")
-        )
-        ,
-    category: "Fun",
-}
+// module.exports.slash = {
+//     data: new SlashCommandBuilder()
+//         .setName('reaction-test')
+//         .setDescription("Take a test on how fast your reaction")
+//         .addSubcommand(option =>
+//             option
+//             .setName('click-button')
+//             .setDescription("Click the button as fast it turns green")
+//         )
+//         ,
+//     category: "Fun",
+// }
